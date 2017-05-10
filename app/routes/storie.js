c61c747e75c9b3359faeae2d7ac6a2f816b6540e
daniele_sciensa <daniele.melo@soaexpert.com.br>
2017-05-10 11:46:41 -0300

    Delete storie.

--- app/routes/storie.js
+++ app/routes/storie.js
@@ -1,7 +1,17 @@
 import Ember from 'ember';
 
 export default Ember.Route.extend({
+
   model(){
     return this.store.findAll('storie');
+  },
+
+  actions: {
+  deleteStories(storie) {
+    let confirmation = confirm('Are you sure?');
+    if (confirmation) {
+      storie.destroyRecord();
+    }
   }
+}
 });
