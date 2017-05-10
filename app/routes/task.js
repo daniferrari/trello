c64471226ca41b8ac02d0124579e0507594578d3
daniele_sciensa <daniele.melo@soaexpert.com.br>
2017-05-10 13:48:10 -0300

    Deleting task.

--- app/routes/task.js
+++ app/routes/task.js
@@ -4,5 +4,14 @@ export default Ember.Route.extend({
 
   model(){
     return this.store.findAll('task');
+  },
+
+  actions: {
+    deleteTask(task) {
+      let confirmation = confirm('Are you sure?');
+      if (confirmation) {
+        task.destroyRecord();
+      }
+    }
   }
 });
