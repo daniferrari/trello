181804ca81f728973cc942987a0d76f6eea3de14
daniele_sciensa <daniele.melo@soaexpert.com.br>
2017-05-10 14:37:20 -0300

    Making easy to navigate between pages.

--- app/models/task.js
+++ app/models/task.js
@@ -1,6 +1,12 @@
 import DS from 'ember-data';
 
 export default DS.Model.extend({
+
+  isValid: Ember.computed.notEmpty('name'),
+  disabled: Ember.computed.not('isValid'),
+  // storyIdValid: Ember.computed.notEmpty('storyId'),
+  // isValidd: Ember.computed.and('nameValid', 'storyIdValid'),
+
   name: DS.attr('string'),
   storyId: DS.belongsTo('story')
 });
